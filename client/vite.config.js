71f95f8671ed4d07b4542082c906964ca238cd73
Ubuntu <ubuntu@ip-172-31-5-195.ap-south-1.compute.internal>
2025-07-04 14:21:01 +0000

    deployed

--- client/vite.config.js
+++ client/vite.config.js
@@ -5,6 +5,9 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server: {
+    allowedHosts: ['shop.merahul.top'],
+    host: true,
+    port: 5173,
     proxy: {
       "/api": {
         target: "http://localhost:5002"
